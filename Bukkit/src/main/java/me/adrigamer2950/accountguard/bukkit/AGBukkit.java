8ff003f10b016aeab35e71843a2179fc2f85fcae
Adrigamer2950 <adrigamer2950@gmail.com>
2024-01-24 17:36:32 +0100

    Basic Config file

--- Bukkit/src/main/java/me/adrigamer2950/accountguard/bukkit/AGBukkit.java
+++ Bukkit/src/main/java/me/adrigamer2950/accountguard/bukkit/AGBukkit.java
@@ -1,9 +1,12 @@
 package me.adrigamer2950.accountguard.bukkit;
 
 import me.adrigamer2950.accountguard.bukkit.commands.MainCommand;
+import me.adrigamer2950.accountguard.bukkit.config.Config;
 import me.adrigamer2950.accountguard.bukkit.database.Database;
 import me.adrigamer2950.accountguard.bukkit.database.DatabaseType;
 import me.adrigamer2950.adriapi.api.command.manager.CommandManager;
+import me.adrigamer2950.adriapi.api.config.manager.ConfigManager;
+import me.adrigamer2950.adriapi.api.config.yaml.YamlConfig;
 import me.adrigamer2950.adriapi.api.logger.APILogger;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -14,12 +17,24 @@ public final class AGBukkit extends JavaPlugin {
     public final APILogger LOGGER = new APILogger("&dAccount&aGuard", null);
 
     private CommandManager commandManager;
+    private ConfigManager configManager;
+    private YamlConfig configFile;
 
     public Database database;
+    public Config config;
 
     @Override
     public void onEnable() {
-        database = Database.getDatabase(this, DatabaseType.YAML);
+        this.configFile = new YamlConfig(this.getDataFolder().getAbsolutePath(), "config", this, true, true);
+        this.configManager = new ConfigManager(this);
+
+        this.configManager.registerConfigFile(this.configFile);
+
+        this.configManager.createConfigFiles();
+
+        this.config = new Config(this.configFile);
+
+        this.database = Database.getDatabase(this, DatabaseType.YAML);
 
         this.commandManager = new CommandManager(this);
         this.commandManager.registerCommand(new MainCommand(this, "accountguard", List.of("ag")));
@@ -30,6 +45,10 @@ public final class AGBukkit extends JavaPlugin {
     @Override
     public void onDisable() {
         this.commandManager = null;
+
+        this.config = null;
+        this.configManager = null;
+        this.configFile = null;
 
         database.saveData();
         this.database = null;
