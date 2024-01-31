a98d9c1145681115851e31e4841df574184f0fd0
Adrigamer2950 <adrigamer2950@gmail.com>
2024-01-31 19:15:01 +0100

    [Common] Implemented getDatabase method on AccountGuard

--- common/src/main/java/me/adrigamer2950/accountguard/common/AccountGuard.java
+++ common/src/main/java/me/adrigamer2950/accountguard/common/AccountGuard.java
@@ -1,10 +1,14 @@
 package me.adrigamer2950.accountguard.common;
 
+import me.adrigamer2950.accountguard.common.config.Config;
+import me.adrigamer2950.accountguard.common.database.Database;
 import me.adrigamer2950.accountguard.common.logger.APILogger;
 
 public interface AccountGuard {
 
     APILogger getAPILogger();
+
+    Database getDatabase();
 
     void reloadConfig();
 
