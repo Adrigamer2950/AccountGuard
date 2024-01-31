b1335b13fcbcfc0ae22cfb98ffc58bd29359f4d1
Adrigamer2950 <adrigamer2950@gmail.com>
2024-01-31 18:56:41 +0100

    [Common] Made custom loggers

---
+++ common/src/main/java/me/adrigamer2950/accountguard/common/logger/APILogger.java
@@ -0,0 +1,48 @@
+package me.adrigamer2950.accountguard.common.logger;
+
+import java.util.logging.Logger;
+
+/**
+ * Main Logger class
+ */
+public class APILogger {
+
+    private final String name;
+    private final APILogger parent;
+
+    private final Logger javaLogger;
+
+    public APILogger(String name, APILogger parent) {
+        this.name = name;
+        this.parent = parent;
+
+        this.javaLogger = Logger.getGlobal();
+    }
+
+    public void info(String message) {
+        this.javaLogger.info(
+                String.format("&r[%s&r] %s",
+                        parent == null ? name : parent.name + " - " + name,
+                        message
+                )
+        );
+    }
+
+    public void warn(String message) {
+        this.javaLogger.warning(
+                String.format("&r[%s&r] %s",
+                        parent == null ? name : parent.name + " - " + name,
+                        message
+                )
+        );
+    }
+
+    public void error(String message) {
+        this.javaLogger.severe(
+                String.format("&r[%s&r] %s",
+                        parent == null ? name : parent.name + " - " + name,
+                        message
+                )
+        );
+    }
+}
---
+++ common/src/main/java/me/adrigamer2950/accountguard/common/logger/SubLogger.java
@@ -0,0 +1,10 @@
+package me.adrigamer2950.accountguard.common.logger;
+
+/**
+ * Sub Logger class
+ */
+public class SubLogger extends APILogger {
+    public SubLogger(String name, APILogger parent) {
+        super(name, parent);
+    }
+}
