c6f742340c1d655d11ead2df7918c996c56ec63d
Adrigamer2950 <adrigamer2950@gmail.com>
2024-01-31 19:41:06 +0100

    [Common] Implemented getPluginMessages method

--- common/src/main/java/me/adrigamer2950/accountguard/common/AccountGuard.java
+++ common/src/main/java/me/adrigamer2950/accountguard/common/AccountGuard.java
@@ -1,6 +1,7 @@
 package me.adrigamer2950.accountguard.common;
 
 import me.adrigamer2950.accountguard.common.config.Config;
+import me.adrigamer2950.accountguard.common.config.Messages;
 import me.adrigamer2950.accountguard.common.database.Database;
 import me.adrigamer2950.accountguard.common.logger.APILogger;
 
@@ -11,6 +12,8 @@ public interface AccountGuard {
     Database getDatabase();
 
     Config getPluginConfig();
+
+    Messages getPluginMessages();
 
     void reloadConfig();
 
