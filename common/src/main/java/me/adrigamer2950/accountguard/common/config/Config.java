55c6a755cf437f10897cb3f912bc6dc469643a8d
Adrigamer2950 <adrigamer2950@gmail.com>
2024-01-31 19:49:10 +0100

    [Common] Rewritten Config class

--- common/src/main/java/me/adrigamer2950/accountguard/common/config/Config.java
+++ common/src/main/java/me/adrigamer2950/accountguard/common/config/Config.java
@@ -1,15 +1,9 @@
 package me.adrigamer2950.accountguard.common.config;
 
-public abstract class Config {
-
-    public final String Prefix;
-
-    public final DatabaseConfig Database;
-
-    protected Config(String Prefix, DatabaseConfig database) {
-        this.Prefix = Prefix;
-        this.Database = database;
-    }
+public record Config (
+        String Prefix,
+        DatabaseConfig Database
+) {
 
     public record DatabaseConfig(String DRIVER, String MYSQL_HOSTNAME, int MYSQL_PORT, String MYSQL_DATABASE,
                                  String MYSQL_USERNAME, String MYSQL_PASSWORD, String REDIS_HOSTNAME, int REDIS_PORT,
