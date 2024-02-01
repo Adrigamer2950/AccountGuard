7318c5b562c43a161ba492cc5e040ccbfa9606c0
Adrigamer2950 <adrigamer2950@gmail.com>
2024-02-01 19:40:59 +0100

    [Common] Made Permissions class

---
+++ common/src/main/java/me/adrigamer2950/accountguard/common/permissions/Permissions.java
@@ -0,0 +1,11 @@
+package me.adrigamer2950.accountguard.common.permissions;
+
+public class Permissions {
+
+    public static final String ALL = "accountguard.*";
+    public static final String RELOAD = "accountguard.admin.reload";
+    public static final String ADD_IPS = "accountguard.admin.add";
+    public static final String REMOVE_IPS = "accountguard.admin.remove";
+    public static final String LIST_IPS = "accountguard.admin.list";
+    public static final String COMMAND = "accountguard.command";
+}
