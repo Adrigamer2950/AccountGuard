9f2218a5bfe8d3d4a5310e04a7bc33332b737076
Adrigamer2950 <adrigamer2950@gmail.com>
2024-02-07 22:56:57 +0100

    [Velocity] Modified OfflinePlayerDatabase

--- Velocity/src/main/java/me/adrigamer2950/accountguard/velocity/database/yaml/OfflinePlayerDatabase.java
+++ Velocity/src/main/java/me/adrigamer2950/accountguard/velocity/database/yaml/OfflinePlayerDatabase.java
@@ -2,6 +2,7 @@ package me.adrigamer2950.accountguard.velocity.database.yaml;
 
 import me.adrigamer2950.accountguard.common.AccountGuard;
 
+import java.io.IOException;
 import java.util.Set;
 import java.util.UUID;
 
@@ -9,6 +10,24 @@ public class OfflinePlayerDatabase extends YAMLDatabase {
 
     public OfflinePlayerDatabase(AccountGuard plugin) {
         super("playerdata", plugin);
+    }
+
+    @Override
+    public void loadData() {
+        try {
+            this.yaml.reload();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void saveData() {
+        try {
+            this.yaml.save();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void setUUID(String username, UUID uuid) {
