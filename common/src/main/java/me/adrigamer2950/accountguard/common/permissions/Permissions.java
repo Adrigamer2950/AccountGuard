b1855f5652f1988829199726334ec47b1431194b
Adrigamer2950 <adrigamer2950@gmail.com>
2024-08-12 01:13:00 +0200

    feat(common): Add Permissions class

---
+++ common/src/main/java/me/adrigamer2950/accountguard/common/permissions/Permissions.java
@@ -0,0 +1,13 @@
+package me.adrigamer2950.accountguard.common.permissions;
+
+public class Permissions {
+
+    public static final String ALL = "accountguard.*";
+    public static final String RELOAD = "accountguard.reload";
+    public static final String ADD_IP_OWN = "accountguard.add.own";
+    public static final String ADD_IP_OTHER = "accountguard.add.other";
+    public static final String REMOVE_IP_OWN = "accountguard.remove.own";
+    public static final String REMOVE_IP_OTHER = "accountguard.remove.other";
+    public static final String LIST_IP_OWN = "accountguard.list.own";
+    public static final String LIST_IP_OTHER = "accountguard.list.other";
+}
